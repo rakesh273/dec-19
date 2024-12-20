a355dee6e991658c8cb2db15580d3090f3ae4f58
rakesh273 <yelarakesh@gmail.com>
2024-12-20 14:15:55 +0530

    Dec 19

---
+++ rakesh-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from "./components/Home";
+import Movies from "./components/Movies";
+export default function App() {
+  return (
+    <div>
+     
+      <Movies/>
+    </div>
+  )
+}
